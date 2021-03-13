3ff5e4e32c00d2ea435b4e3cc6e86dee4bd494e7
betlewski <szymonbetlewski@wp.pl>
2021-03-13 15:24:52 +0100

    Created model classes for project, student and task

---
+++ src/main/java/pl/edu/utp/pz1/model/Task.java
@@ -0,0 +1,42 @@
+package pl.edu.utp.pz1.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@NoArgsConstructor
+@Data
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer taskId;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(length = 1000)
+    private String description;
+
+    private Integer sequence;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createDateTime;
+
+    @ManyToOne
+    @JoinColumn(name = "projectId")
+    private Project project;
+
+    public Task(String name, String description, Integer sequence) {
+        this.name = name;
+        this.description = description;
+        this.sequence = sequence;
+    }
+
+}
