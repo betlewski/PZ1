b7ee34a86909732518133bf574acb75cfc1bf12a
betlewski <szymonbetlewski@wp.pl>
2021-03-16 14:36:30 +0100

    Created string utils class

---
+++ src/main/java/pl/edu/utp/pz1/util/StringUtils.java
@@ -0,0 +1,9 @@
+package pl.edu.utp.pz1.util;
+
+public class StringUtils {
+
+    public static boolean isNullOrEmpty(String text) {
+        return text == null || text.trim().isEmpty();
+    }
+
+}
